673bf4d8e89326262fb3304e35465f8e8eb8a76d
Justin Mathew <jmathew2111@yahoo.com>
2020-06-10 02:46:12 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,34 +1,28 @@
-array = [1, 2, 16, 3, 4, 5, 6]
-
- def my_select(collection)
-  new_selection = [] 
-   i = 0
-   while i < collection.length
- 	  if yield(collection[i])
- 	   	new_selection << collection[i]
-	  end
- 	  i += 1
-     end
-  new_selection #return the modified array
- end
-
-my_select(array.sort) {|num| num.even?}
+list = ["ab", "cd", "ef"]
 
 def my_each(array)
   i = 0
   while i < array.length
-    yield array[i]
-      i += 1
-     end
-     array
- end
-
- my_each(array) {|i| i}
-
-def yield_example
-	puts "Hi I'm having a nice day."
-	yield if block_given?
-	puts "How did you get here?!"
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
+  end
 end
 
-puts yield_example {puts "I AM A YIELD BLOCK INTERUPTING UR CODE"} 
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
+
+# Use the collection method 
+# To make the *return-value* the answer you want
+# instead of "=> nil"
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
