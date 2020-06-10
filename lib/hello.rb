c59a79990940ff4049fd106433696b67ee711664
Justin Mathew <jmathew2111@yahoo.com>
2020-06-10 02:43:13 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,35 @@
-def hello_t
+array = [1, 2, 16, 3, 4, 5, 6]
 
+# def my_select(collection)
+#   new_selection = [] 
+#   i = 0
+#   while i < collection.length
+# 	  if yield(collection[i])
+# 	   	new_selection << collection[i]
+# 	  end
+# 	  i += 1
+#     end
+#   new_selection #return the modified array
+# end
+
+# my_select(array.sort) {|num| num.even?}
+
+# def my_each(array)
+#   i = 0
+#   while i < array.length
+#     yield array[i]
+#       i += 1
+#     end
+#     array
+# end
+
+# my_each(array) {|i| i}
+
+#######HEY BEB LOOK HERE!################
+def yield_example
+	puts "Hi I'm having a nice day."
+	yield if block_given?
+	puts "How did you get here?!"
 end
 
-# call your method here!
-
+puts yield_example {puts "I AM A YIELD BLOCK INTERUPTING UR CODE"} #<<< remove 
