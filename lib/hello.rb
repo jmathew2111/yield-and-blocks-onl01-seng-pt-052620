b749bda0e76525832f0a302485de654e803a860f
Justin Mathew <jmathew2111@yahoo.com>
2020-06-10 02:43:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,31 +1,5 @@
-array = [1, 2, 16, 3, 4, 5, 6]
+array = [2, 3, 78, 32, 43, 55, 61]
 
-# def my_select(collection)
-#   new_selection = [] 
-#   i = 0
-#   while i < collection.length
-# 	  if yield(collection[i])
-# 	   	new_selection << collection[i]
-# 	  end
-# 	  i += 1
-#     end
-#   new_selection #return the modified array
-# end
-
-# my_select(array.sort) {|num| num.even?}
-
-# def my_each(array)
-#   i = 0
-#   while i < array.length
-#     yield array[i]
-#       i += 1
-#     end
-#     array
-# end
-
-# my_each(array) {|i| i}
-
-#######HEY BEB LOOK HERE!################
 def yield_example
 	puts "Hi I'm having a nice day."
 	yield if block_given?
