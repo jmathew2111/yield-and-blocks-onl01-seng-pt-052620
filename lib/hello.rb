dabbbc53d06b6f9a23d669e71507fcee111645f3
Justin Mathew <jmathew2111@yahoo.com>
2020-06-10 02:43:50 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++
@@ -1,9 +0,0 @@
-array = [2, 3, 78, 32, 43, 55, 61]
-
-def yield_example
-	puts "Hi I'm having a nice day."
-	yield if block_given?
-	puts "How did you get here?!"
-end
-
-puts yield_example {puts "I AM A YIELD BLOCK INTERUPTING UR CODE"} #<<< remove 
